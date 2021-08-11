33bdbd6ee5a8ff3d8691fe522a959e01ae593fb0
gundeep06 <71358874+gundeep06@users.noreply.github.com>
2021-08-10 22:23:43 -0400

    Add files via upload

---
+++ config.js
@@ -0,0 +1,24 @@
+import firebase from "firebase";
+
+var firebaseConfig = {
+    apiKey: "AIzaSyBsHC6OKOKZBBBUcGt2NjPIgqT_IwLjHFw",
+    authDomain: "project-60-271ef.firebaseapp.com",
+    databaseURL: "https://project-60-271ef-default-rtdb.firebaseio.com",
+    projectId: "project-60-271ef",
+    storageBucket: "project-60-271ef.appspot.com",
+    messagingSenderId: "1031700221634",
+    appId: "1:1031700221634:web:62dab1bc43c4c6d85d755b"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+  export default firebase.database()
+
+
+ 
+
+
+ 
+ 
+
+  
